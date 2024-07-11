ac049d3c8cb43ceb5b8b6b7e024d27de47426065
Javiera Toro Grey <111334548+JavieraTGrey@users.noreply.github.com>
2024-07-10 23:18:15 -0400

    Agrega espectros segun tipo espectral

--- star.py
+++ star.py
@@ -1,17 +1,25 @@
-import numpy as np
 from astropy.constants import sigma_sb, L_sun, M_sun, R_sun
+from astropy.io import fits
 import astropy.units as u
+from io import BytesIO
+import numpy as np
 
 
 class STAR:
 
-    def __init__(self, mass, born):
+    def __init__(self, mass, born, spectra):
 
+        self.spectra = spectra
         self.mass = mass*M_sun
         self.born = born * u.Gyr
         self.stage = self.born
         self.branch = 'MS'
         self.properties()
+        self.get_star_spectrum()
+
+        self.wavelength = self.wavelengths[self.wavelengths < 10000]
+        self.spectrum = self.spectrum[self.wavelengths < 10000]
+        self.flux = self.flux[self.wavelengths < 10000]
 
     def properties(self):
 
@@ -30,21 +38,46 @@ class STAR:
         self.radii = R_sun * (self.mass / M_sun)**0.8
         denominator = (4 * np.pi * (self.radii**2) * sigma)
         self.temperature = ((self.luminosity / denominator) ** 0.25)
-        
-        if self.temperature.value >= 33000:
+
+        if (self.mass/M_sun) > 16:
             self.spectral_type = 'O'
-        elif self.temperature.value >= 10000:
+        elif (self.mass/M_sun) > 2.1:
             self.spectral_type = 'B'
-        elif self.temperature.value >= 7300:
+        elif (self.mass/M_sun) > 1.4:
             self.spectral_type = 'A'
-        elif self.temperature.value >= 6000:
+        elif (self.mass/M_sun) > 1.04:
             self.spectral_type = 'F'
-        elif self.temperature.value >= 5300:
+        elif (self.mass/M_sun) > 0.8:
             self.spectral_type = 'G'
-        elif self.temperature.value >= 3900:
+        elif (self.mass/M_sun) > 0.45:
             self.spectral_type = 'K'
         else:
             self.spectral_type = 'M'
+
+    def read_spectrum_from_fits(self, data):
+        with fits.open(BytesIO(data)) as hdul:
+            header = hdul[0].header
+            spectrum_data = hdul[0].data
+
+            wavelengths = np.arange(header['CRVAL1'],
+                                    header['CRVAL1'] + header['CDELT1'] * len(spectrum_data),
+                                    header['CDELT1'])
+            return wavelengths[:len(spectrum_data)], spectrum_data
+
+    def get_star_spectrum(self):
+        new_type = [type for type in list(self.spectra) if type.startswith(self.spectral_type)]
+        new_spectral_type = np.random.choice(new_type)
+        if new_spectral_type in self.spectra:
+
+            data = self.spectra[new_spectral_type]
+            self.wavelengths, self.spectrum = self.read_spectrum_from_fits(data)
+
+            self.total_lum = np.trapz(self.spectrum, x=self.wavelengths)
+
+            self.flux = self.spectrum / self.total_lum
+
+        else:
+            raise ValueError(f"Spectral type {new_spectral_type} not available in spectra.")
 
     def update(self, t):
         # T es edad del universo!
